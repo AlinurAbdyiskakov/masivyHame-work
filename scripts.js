62bea08bd05c7aab9df3563f9c025cd4fa5016ca
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-01-31 14:58:17 +0600

    add let example3

---
+++ scripts.js
@@ -0,0 +1,8 @@
+let dw = text => document.write(text + "<br>")
+let example1 = 123.436577;
+dw(example1.toFixed(2));
+
+ let example2 = [["Apple"],["Malina"],["Banana"],["Jyzym"],["Ashpurt"]];
+dw(example2);
+let example3 = "Hello World , Hello Alinur";
+dw(example3.length);
